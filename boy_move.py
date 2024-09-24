e5cda251a548ae4548613d808376de955bc26ece
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-09-24 11:57:05 +0900

    add run_circle

--- boy_move.py
+++ boy_move.py
@@ -5,6 +5,15 @@ open_canvas()
 
 boy = load_image('character.png')
 
+r = 300
+
+def run_circle():
+    for d in range(0,360):
+        x = r * math.cos(math.radians(d))
+        y = r * math.sin(math.radians(d))
+    pass
+
+
 while True:
     run_circle()
     run_rectangle()
